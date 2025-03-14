e3c9cac9d9c288f365f3de40087be6f3b34318d7
L145 <147063982+legelff@users.noreply.github.com>
2025-03-14 01:13:22 +0100

    define grid layout

---
+++ goongoonarray-frontend/src/components/ProfileComponent/ProfileComponent.jsx
@@ -0,0 +1,12 @@
+import './ProfileComponent.css';
+import React from 'react';
+
+function ProfileComponent(props) {
+    return (
+        <div className='profile-container' id={`profile-${props.id}`}>
+
+        </div>
+    )
+}
+
+export default ProfileComponent;
